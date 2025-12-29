04a54bc0162c7fb36d20c48af2899670af702047
AjayGuhade <ajay.22210623@viit.ac.in>
2025-12-29 15:48:31 +0530

    feet: added day-02 task completed

---
+++ day-02/practice/api_data_fetcher.py
@@ -0,0 +1,25 @@
+import requests
+
+
+api_url = "https://pokeapi.co/api/v2/"
+
+
+def get_pokemon_info(name):
+    url = f"{api_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+      pokemon_data = response.json()  
+      return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+        return None
+
+pokemaon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemaon_name)
+
+if pokemon_info:
+    print(f"{pokemon_info["name"]}")
+    print(f"{pokemon_info["id"]}")
+    print(f"{pokemon_info["height"]}")
+    print(f"{pokemon_info["weight"]}") 
